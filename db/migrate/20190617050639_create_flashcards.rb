1481dc3814c67cea3c2c6861d1eafa651be93e93
Tony Huynh <t.hnh@tuta.io>
2019-06-17 15:09:32 +1000

    generated model

---
+++ db/migrate/20190617050639_create_flashcards.rb
@@ -0,0 +1,10 @@
+class CreateFlashcards < ActiveRecord::Migration[5.2]
+  def change
+    create_table :flashcards do |t|
+      t.string :front, limit: 50
+      t.string :back, limit: 200
+
+      t.timestamps
+    end
+  end
+end
