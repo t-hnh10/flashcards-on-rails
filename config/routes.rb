72067a895b1a1fc152e498a786f2f0aa64ec16c7
Tony Huynh <t.hnh@tuta.io>
2019-06-17 14:40:06 +1000

    added root route and db settings

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'flashcards/index'
+  root to: "flashcards#index"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
