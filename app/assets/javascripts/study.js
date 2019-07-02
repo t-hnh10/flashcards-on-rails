11259fd6a49df4416a401f1cf53fc89fb6b376fe
Tony Huynh <t.hnh@tuta.io>
2019-07-02 23:28:29 +1000

    added transliteration button

--- app/assets/javascripts/study.js
+++ app/assets/javascripts/study.js
@@ -1,7 +1,16 @@
-function myFunction() {
-    var x = document.getElementById("showAnswer");
+function showDefinition() {
+    var x = document.getElementById("showDef");
     if (x.style.display === "none") {
-        x.style.display = "block";
+        x.style.display = "inline";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function showTransliteration() {
+    var x = document.getElementById("showTrans");
+    if (x.style.display === "none") {
+        x.style.display = "inline";
     } else {
         x.style.display = "none";
     }
