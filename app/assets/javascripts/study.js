74f76313214e70aeaf6cb4b6a83bca25b3a05e8c
Tony Huynh <t.hnh@tuta.io>
2019-07-02 16:19:06 +1000

    modified js variable name

--- app/assets/javascripts/study.js
+++ app/assets/javascripts/study.js
@@ -1,5 +1,5 @@
 function myFunction() {
-    var x = document.getElementById("myDIV");
+    var x = document.getElementById("showAnswer");
     if (x.style.display === "none") {
         x.style.display = "block";
     } else {
